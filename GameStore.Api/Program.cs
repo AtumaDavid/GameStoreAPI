4e4c64c2e17de97a9526efbc7e1cfdc282cc1760
Atuma David <atumadavid35@gmail.com>
2024-11-23 11:01:30 +0100

    post request

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,6 +3,8 @@ using GameStore.Api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndPoint = "GetGame";
+
 List<GameDto> games = [
     new (
         1,
@@ -31,7 +33,21 @@ List<GameDto> games = [
 app.MapGet("games", () => games);
 
 // GET GAME "/games/id"
-app.MapGet("games/{id}",(int id) => games.Find(game => game.Id == id));
+app.MapGet("games/{id}",(int id) => games.Find(game => game.Id == id)).WithName(GetGameEndPoint);
+
+// POST GAME "/game"
+app.MapPost("games", (CreateGameDto newGame) => {
+    GameDto game = new (
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndPoint, new {id = game.Id}, game);
+});
 
 // app.MapGet("/", () => "Hello World!");
 
