8b918b8f6e617228b44fe471f09ce01194359798
Atuma David <atumadavid35@gmail.com>
2024-11-25 13:46:05 +0100

    endpoints

---
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,93 @@
+﻿using GameStore.Api.Dtos;
+using Microsoft.AspNetCore.Builder;
+using System.Collections.Generic;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameEndPoint = "GetGame";
+
+    private static List<GameDto> games = new List<GameDto>
+    {
+        new GameDto(
+            1,
+            "Street Fighter II",
+            "Fighting",
+            19.99M,
+            new DateOnly(1992, 7, 5)
+        ),
+        new GameDto(
+            2,
+            "Final Fantasy XIV",
+            "Roleplaying",
+            59.99M,
+            new DateOnly(2010, 9, 30)
+        ),
+        new GameDto(
+            3,
+            "Street Fighter III",
+            "Fighting",
+            29.99M,
+            new DateOnly(1995, 7, 5)
+        ),
+    };
+
+    public static WebApplication MapGamesEndpoints(this WebApplication app)
+    {
+        // GET GAMES "/games"
+        app.MapGet("games", () => games);
+
+        // GET GAME "/games/id"
+        app.MapGet("games/{id}", (int id) =>
+        {
+            GameDto? game = games.Find(game => game.Id == id);
+            return game is null ? Results.NotFound() : Results.Ok(game);
+        }).WithName(GetGameEndPoint);
+
+        // POST GAME "/games"
+        app.MapPost("games", (CreateGameDto newGame) =>
+        {
+            GameDto game = new GameDto(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+            games.Add(game);
+
+            return Results.CreatedAtRoute(GetGameEndPoint, new { id = game.Id }, game);
+        });
+
+        // PUT GAME "/games/id"
+        app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+
+            games[index] = new GameDto(
+                id,
+                updatedGame.Name,
+                updatedGame.Genre,
+                updatedGame.Price,
+                updatedGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        // DELETE GAME "/games/id"
+        app.MapDelete("games/{id}", (int id) =>
+        {
+            games.RemoveAll(game => game.Id == id);
+            return Results.NoContent();
+        });
+
+        return app;
+    }
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,54 +1,86 @@
-using GameStore.Api.Dtos;
+// using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string GetGameEndPoint = "GetGame";
+// const string GetGameEndPoint = "GetGame";
 
-List<GameDto> games = [
-    new (
-        1,
-        "Street Fighter II",
-        "Fighting",
-        19.99M, //decimal
-        new DateOnly(1992, 7, 5)
-    ),
-    new (
-        2,
-        "Final Fantasy XIV",
-        "Roleplaying",
-        59.99M,
-        new DateOnly(2010, 9, 30)
-    ),
-    new (
-        3,
-        "Street Fighter III",
-        "Fighting",
-        29.99M,
-        new DateOnly(1995, 7, 5)
-    ),
-];
+// List<GameDto> games = [
+//     new (
+//         1,
+//         "Street Fighter II",
+//         "Fighting",
+//         19.99M, //decimal
+//         new DateOnly(1992, 7, 5)
+//     ),
+//     new (
+//         2,
+//         "Final Fantasy XIV",
+//         "Roleplaying",
+//         59.99M,
+//         new DateOnly(2010, 9, 30)
+//     ),
+//     new (
+//         3,
+//         "Street Fighter III",
+//         "Fighting",
+//         29.99M,
+//         new DateOnly(1995, 7, 5)
+//     ),
+// ];
 
-// GET GAMES "/games"
-app.MapGet("games", () => games);
+// // GET GAMES "/games"
+// app.MapGet("games", () => games);
 
-// GET GAME "/games/id"
-app.MapGet("games/{id}",(int id) => games.Find(game => game.Id == id)).WithName(GetGameEndPoint);
+// // GET GAME "/games/id"
+// app.MapGet("games/{id}",(int id) => {
+//      GameDto? game = games.Find(game => game.Id == id);
+//      return game is null ? Results.NotFound() : Results.Ok(game) ;
+//     }).WithName(GetGameEndPoint);
 
-// POST GAME "/game"
-app.MapPost("games", (CreateGameDto newGame) => {
-    GameDto game = new (
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-    games.Add(game);
+// // POST GAME "/game"
+// app.MapPost("games", (CreateGameDto newGame) => {
+//     GameDto game = new (
+//         games.Count + 1,
+//         newGame.Name,
+//         newGame.Genre,
+//         newGame.Price,
+//         newGame.ReleaseDate
+//     );
+//     games.Add(game);
 
-    return Results.CreatedAtRoute(GetGameEndPoint, new {id = game.Id}, game);
-});
+//     return Results.CreatedAtRoute(GetGameEndPoint, new {id = game.Id}, game);
+// });
+
+// // PUT GAME "/game/id"
+// app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
+//     var index = games.FindIndex(game => game.Id == id);
+
+//     if (index == -1){
+//         return Results.NotFound();
+//     }
+
+//     games[index] = new GameDto(
+//         id,
+//         updatedGame.Name,
+//         updatedGame.Genre,
+//         updatedGame.Price,
+//         updatedGame.ReleaseDate
+//     );
+
+//     return Results.NoContent();
+// });
+
+// // DELETE GAME "/games/id"
+// app.MapDelete("games/{id}", (int id) => {
+//     games.RemoveAll(game => game.Id == id);
+
+//     return Results.NoContent();
+// });
 
 // app.MapGet("/", () => "Hello World!");
+
+app.MapGamesEndpoints();
 
 app.Run();
